ddc912aa5674afd817888763d7a6aa05c18c1212
edi-prasetyo <ediprasetiyo2@gmail.com>
2024-02-28 11:40:56 +0700

    Update Invoice

--- config/app.php
+++ config/app.php
@@ -196,6 +196,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\DomPDF\ServiceProvider::class,
+
     ],
 
     /*
@@ -211,6 +213,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+
 
     ])->toArray(),
     // 'aliases' => [
